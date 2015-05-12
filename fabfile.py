59e069a2a0bdefe4f28816ac64b3a1cf08a9edcb
Felipe Holanda <flp9001@gmail.com>
2015-05-12 01:39:23 -0300

    Server IP

--- fabfile.py
+++ fabfile.py
@@ -11,12 +11,14 @@ env.user = 'root'
 env.http_user = 'www-data'
 env.http_group = 'www-data'
 env.hosts = ['104.236.42.196']
-env.base_path = '/var/www'
-env.path = join(env.base_path, 'astrology')
+env.www_path = '/var/www'
+env.path = join(env.www_path, 'astrology')
+env.server_conf = 'astro/config/apache2/astro.conf'
+
 env.venv_name = 'venv'
-env.venv = join(env.base_path, env.venv_name)
+env.venv_path = join(env.www_path, env.venv_name)
 env.github_repo = 'git@github.com:flp9001/astrology.git'
-env.activate = 'source {venv}/bin/activate'.format(**env)
+env.activate = 'source {venv_path}/bin/activate'.format(**env)
 
 
 @contextmanager
@@ -37,13 +39,15 @@ def py_dep():
 
 @task
 def restart():
+    with cd(env.path):
+        run('cp {server_conf} /etc/apache2/sites-enabled/'.format(**env))
     run('service apache2 restart')
 
 
 @task
 def initial_setup():
-    run('mkdir -p {base_path}'.format(**env))
-    with cd(env.base_path):
+    run('mkdir -p {www_path}'.format(**env))
+    with cd(env.www_path):
         run('git clone {github_repo}'.format(**env))
         run('virtualenv {venv_name}'.format(**env))    
     os_dep()
